e60396bb5b4f4bbb3720f2e1d5aa43f03c96c615
zsc <755717181@qq.com>
2019-12-12 21:28:54 +0800

    init

---
+++ spec/image-uploader-sm-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ImageUploaderSmView from '../lib/image-uploader-sm-view';
+
+describe('ImageUploaderSmView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
